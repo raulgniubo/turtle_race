821c83c74731c93acea881c55aaad3831f33caed
Raul Gutierrez Niubo <raulgniubo@gmail.com>
2021-02-03 09:36:18 -0500

    all 6 turtles added to the canvas

--- PyCharm_Project/main.py
+++ PyCharm_Project/main.py
@@ -3,10 +3,13 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
-print(user_bet)
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
 
-tim = Turtle(shape="turtle")
-tim.penup()
-tim.goto(x=-230, y=-100)
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.color(colors[turtle_index])
+    tim.penup()
+    tim.goto(x=-230, y=y_positions[turtle_index])
 
 screen.exitonclick()
