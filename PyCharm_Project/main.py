d7c4f4a20484a8242e93f441f551cedea6d8312c
Raul Gutierrez Niubo <raulgniubo@gmail.com>
2021-02-03 09:24:53 -0500

    pop up screen added for the bet

--- PyCharm_Project/main.py
+++ PyCharm_Project/main.py
@@ -2,6 +2,8 @@ from turtle import Turtle, Screen
 
 tim = Turtle()
 screen = Screen()
-screen.setup(500, 400)
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+print(user_bet)
 
 screen.exitonclick()
